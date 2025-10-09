124b373ce8048c8446c23bb20babe4fc33b3b2b0
Roger Schoellgen <roger.s@infinitivegames.com>
2025-10-08 19:11:47 -0700

    Cleaned up run-all output.

--- run-all.bat
+++ run-all.bat
@@ -1,21 +1,32 @@
 @echo off
-setlocal EnableDelayedExpansion
-rem get ESC for ANSI color (Win10+)
+setlocal EnableExtensions EnableDelayedExpansion
+
+rem Get ESC for ANSI color (Win10+)
 for /f "delims=" %%A in ('echo prompt $E^| cmd') do set "ESC=%%A"
 set "RED=%ESC%[31m"
 set "RESET=%ESC%[0m"
 
-for /r "data" %%f in (*.asm) do (
-  if "%%~xf"==".asm" (
-    echo %%f
-    nasm %%f
-    8086_decoder %%~df%%~pf%%~nf > %%~df%%~pf%%~nf_decoded.output_asm
-    nasm %%~df%%~pf%%~nf_decoded.output_asm
-    fc %%~df%%~pf%%~nf_decoded %%~df%%~pf%%~nf >nul
-    if errorlevel 1 (
-      echo %RED%DIFF detected in %%~f%RESET%
-      exit /b 1
-    )
+rem Walk all .asm files under .\data (recursively)
+for /r "data" %%F in (*.asm) do (
+  rem Build an output directory next to the file: <dir>\output\
+  set "outdir=%%~dpFoutput\"
+  if not exist "!outdir!" mkdir "!outdir!"
+
+  rem Base name without extension
+  set "base=!outdir!%%~nF"
+
+  echo Processing: "%%F"
+  nasm "%%F" -o "!base!"
+
+  8086_decoder "!base!" > "!base!_decoded.output_asm"
+  nasm "!base!_decoded.output_asm" -o "!base!_decoded"
+
+  rem Binary compare
+  fc /b "!base!_decoded" "!base!" >nul
+  if errorlevel 1 (
+    echo !RED!DIFF detected in "%%~F"!RESET!
+    exit /b 1
   )
 )
+
 endlocal
